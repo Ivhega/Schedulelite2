04c1ec57c846704798f21575a9eac278b8155bd2
Adam <adam.devlaming@edu.sait.ca>
2024-07-06 14:58:56 -0600

    Minor updates to home

--- app/auth/(tabs)/home.tsx
+++ app/auth/(tabs)/home.tsx
@@ -4,13 +4,41 @@ import React from 'react'
 
 const home = () => {
   return (
-    <View>
-      <Text>home</Text>
+    <View style={styles.main}>
+      <Text style={styles.heading1}>Welcome to</Text>
+      <Text style={styles.heading2}>SchedulElite!</Text>
     </View>
   )
 }
 
 export default home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    main: {
+        flex: 1,
+        backgroundColor: 'red',
+    },
+    heading1: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 35,
+        fontWeight: 'bold',
+        marginLeft: '23%',
+        marginTop: 60,
+        fontFamily: 'Arial',
+        color: 'yellow',
+    },
+    heading2: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 35,
+        fontWeight: 'bold',
+        marginLeft: '20%',
+        marginTop: 5,
+        fontFamily: 'Arial',
+        color: 'yellow',
+    }
+
+
+})
 
